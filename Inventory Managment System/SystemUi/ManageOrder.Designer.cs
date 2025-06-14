7bb34aaee784599947749b5dbe70670eef0b1dc9
King-Santa <kingsanta47@gmail.com>
2025-06-15 03:52:09 +0530

    Manage Order and Supplier Dahboard and created

--- Inventory Managment System/SystemUi/ManageOrder.Designer.cs
+++ Inventory Managment System/SystemUi/ManageOrder.Designer.cs
@@ -28,14 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonCreateOrder = new System.Windows.Forms.Button();
+            this.buttonDeleteOrder = new System.Windows.Forms.Button();
+            this.labelManageOrder = new System.Windows.Forms.Label();
+            this.dataGridViewManageOrder = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewManageOrder)).BeginInit();
             this.SuspendLayout();
+            // 
+            // buttonCreateOrder
+            // 
+            this.buttonCreateOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F);
+            this.buttonCreateOrder.Location = new System.Drawing.Point(203, 102);
+            this.buttonCreateOrder.Name = "buttonCreateOrder";
+            this.buttonCreateOrder.Size = new System.Drawing.Size(127, 36);
+            this.buttonCreateOrder.TabIndex = 36;
+            this.buttonCreateOrder.Text = "Create";
+            this.buttonCreateOrder.UseVisualStyleBackColor = true;
+            // 
+            // buttonDeleteOrder
+            // 
+            this.buttonDeleteOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F);
+            this.buttonDeleteOrder.Location = new System.Drawing.Point(426, 102);
+            this.buttonDeleteOrder.Name = "buttonDeleteOrder";
+            this.buttonDeleteOrder.Size = new System.Drawing.Size(127, 36);
+            this.buttonDeleteOrder.TabIndex = 35;
+            this.buttonDeleteOrder.Text = "Delete";
+            this.buttonDeleteOrder.UseVisualStyleBackColor = true;
+            // 
+            // labelManageOrder
+            // 
+            this.labelManageOrder.AutoSize = true;
+            this.labelManageOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold);
+            this.labelManageOrder.Location = new System.Drawing.Point(266, 31);
+            this.labelManageOrder.Name = "labelManageOrder";
+            this.labelManageOrder.Size = new System.Drawing.Size(181, 29);
+            this.labelManageOrder.TabIndex = 34;
+            this.labelManageOrder.Text = "Manage Order";
+            // 
+            // dataGridViewManageOrder
+            // 
+            this.dataGridViewManageOrder.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewManageOrder.Location = new System.Drawing.Point(157, 189);
+            this.dataGridViewManageOrder.Name = "dataGridViewManageOrder";
+            this.dataGridViewManageOrder.RowHeadersWidth = 51;
+            this.dataGridViewManageOrder.RowTemplate.Height = 24;
+            this.dataGridViewManageOrder.Size = new System.Drawing.Size(411, 150);
+            this.dataGridViewManageOrder.TabIndex = 37;
             // 
             // ManageOrder
             // 
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.ClientSize = new System.Drawing.Size(810, 405);
+            this.Controls.Add(this.dataGridViewManageOrder);
+            this.Controls.Add(this.buttonCreateOrder);
+            this.Controls.Add(this.buttonDeleteOrder);
+            this.Controls.Add(this.labelManageOrder);
             this.Name = "ManageOrder";
             this.Load += new System.EventHandler(this.ManageOrder_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewManageOrder)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -54,5 +105,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonCreateOrder;
+        private System.Windows.Forms.Button buttonDeleteOrder;
+        private System.Windows.Forms.Label labelManageOrder;
+        private System.Windows.Forms.DataGridView dataGridViewManageOrder;
     }
 }
--- Inventory Managment System/SystemUi/SupplierDashboardUI.Designer.cs
+++ Inventory Managment System/SystemUi/SupplierDashboardUI.Designer.cs
@@ -28,12 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelSupplierDashboard = new System.Windows.Forms.Label();
+            this.buttonSupplierStockMovement = new System.Windows.Forms.Button();
+            this.buttonSupplerManageProdect = new System.Windows.Forms.Button();
+            this.buttonSupplerLogout = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelSupplierDashboard
+            // 
+            this.labelSupplierDashboard.AutoSize = true;
+            this.labelSupplierDashboard.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold);
+            this.labelSupplierDashboard.Location = new System.Drawing.Point(226, 46);
+            this.labelSupplierDashboard.Name = "labelSupplierDashboard";
+            this.labelSupplierDashboard.Size = new System.Drawing.Size(246, 29);
+            this.labelSupplierDashboard.TabIndex = 1;
+            this.labelSupplierDashboard.Text = "Supplier Dashboard";
+            // 
+            // buttonSupplierStockMovement
+            // 
+            this.buttonSupplierStockMovement.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSupplierStockMovement.Location = new System.Drawing.Point(374, 142);
+            this.buttonSupplierStockMovement.Name = "buttonSupplierStockMovement";
+            this.buttonSupplierStockMovement.Size = new System.Drawing.Size(188, 33);
+            this.buttonSupplierStockMovement.TabIndex = 3;
+            this.buttonSupplierStockMovement.Text = "StockMovement";
+            this.buttonSupplierStockMovement.UseVisualStyleBackColor = true;
+            // 
+            // buttonSupplerManageProdect
+            // 
+            this.buttonSupplerManageProdect.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSupplerManageProdect.Location = new System.Drawing.Point(109, 142);
+            this.buttonSupplerManageProdect.Name = "buttonSupplerManageProdect";
+            this.buttonSupplerManageProdect.Size = new System.Drawing.Size(188, 33);
+            this.buttonSupplerManageProdect.TabIndex = 4;
+            this.buttonSupplerManageProdect.Text = "Manage Products";
+            this.buttonSupplerManageProdect.UseVisualStyleBackColor = true;
+            // 
+            // buttonSupplerLogout
+            // 
+            this.buttonSupplerLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F);
+            this.buttonSupplerLogout.Location = new System.Drawing.Point(281, 233);
+            this.buttonSupplerLogout.Name = "buttonSupplerLogout";
+            this.buttonSupplerLogout.Size = new System.Drawing.Size(85, 36);
+            this.buttonSupplerLogout.TabIndex = 5;
+            this.buttonSupplerLogout.Text = "Logout";
+            this.buttonSupplerLogout.UseVisualStyleBackColor = true;
+            // 
+            // SupplierDashboardUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(687, 327);
+            this.Controls.Add(this.buttonSupplerLogout);
+            this.Controls.Add(this.buttonSupplerManageProdect);
+            this.Controls.Add(this.buttonSupplierStockMovement);
+            this.Controls.Add(this.labelSupplierDashboard);
+            this.Name = "SupplierDashboardUI";
             this.Text = "SupplierDashboard";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelSupplierDashboard;
+        private System.Windows.Forms.Button buttonSupplierStockMovement;
+        private System.Windows.Forms.Button buttonSupplerManageProdect;
+        private System.Windows.Forms.Button buttonSupplerLogout;
     }
 }
